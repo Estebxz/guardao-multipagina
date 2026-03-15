33605243656793120fded3170fbe554e16e276df
Estebxz <estebanmendezm1995@gmail.com>
2026-03-15 14:53:58 -0500

    fix: bug para saludo arreglado manejando una constante para cada hora del dia sin fallback

--- app/components/common/greeting.tsx
+++ app/components/common/greeting.tsx
@@ -1,19 +1,17 @@
 import { FC } from "react";
 
+const GREETINGS = [
+  { from: 0, to: 6, text: "Que haces despierto?!" },
+  { from: 6, to: 12, text: "Buenos días" },
+  { from: 12, to: 18, text: "Buenas tardes" },
+  { from: 18, to: 24, text: "Buenas noches" },
+] as const;
+
 const Greeting: FC = () => {
-  const currentTime = new Date();
-  const currentHour = currentTime.getHours();
+  const hour = new Date().getHours();
+  const { text } = GREETINGS.find(({ from, to }) => hour >= from && hour < to)!;
 
-  let greeting = "";
-  if (currentHour >= 6 && currentHour < 12) {
-    greeting = "Buenos días";
-  } else if (currentHour > 12 && currentHour < 18) {
-    greeting = "Buenas tardes";
-  } else {
-    greeting = "Buenas noches";
-  }
-
-  return <h1 className="text-7xl font-fusion capitalize">{greeting}</h1>;
+  return <h1 className="text-7xl font-fusion capitalize">{text}</h1>;
 };
 
 export default Greeting;
